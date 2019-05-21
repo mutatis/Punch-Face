ed6aa7916ef2c83c0f6d41f2994975855b0e3fd9
Yaspelus <mutatisguga@gmail.com>
2019-05-20 22:06:56 -0300

    Start
    Tela trocando e play desligando direto

---
+++ Assets/Script/Interface/Set_Background.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Set_Background : MonoBehaviour
+{
+
+    public Sprite[] sprite;
+
+    public Image image;
+
+    int random;
+
+    void Start()
+    {
+        random = Random.Range(0, sprite.Length);
+        image.sprite = sprite[random];
+    }
+}
