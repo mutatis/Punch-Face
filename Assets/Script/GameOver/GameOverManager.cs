d0d465eec48255a61b4a988215e06235c93e618c
Yaspelus <mutatisguga@gmail.com>
2019-07-31 23:03:41 -0300

    Unity ads
    video unity ads is work

---
+++ Assets/Script/GameOver/GameOverManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class GameOverManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(Advertisement.IsReady())
+        {
+            Advertisement.Show();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
